72504d6d9f7922abb6a0b74e9ac525b363215fda
LT-Chan <113198408+LT-Chan@users.noreply.github.com>
2023-10-18 01:56:02 -0400

    Initial Stuff
    Just the basics, WIP.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.intro-content').style.opacity = "1";
+    }, 500); // delay of 500ms before the fade-in begins
+});
+
+
